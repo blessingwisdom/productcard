50021c9b006661701898e6dacd50ed88f073d18e
Blessing Wisdom <88656234+blessingwisdom@users.noreply.github.com>
2022-09-21 05:23:03 +0100

    Going

--- lib/main.dart
+++ lib/main.dart
@@ -4,5 +4,31 @@ void main() {
   runApp(const ProductCard());
 }
 
-stl
+class ProductCard extends StatelessWidget {
+  const ProductCard({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Voces'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xfff2EBE3),
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(),
+      )),
+    );
+  }
+}
